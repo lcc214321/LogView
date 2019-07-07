067b99aef59a17796343ed3a28c98dd820381b95
peng  chen <39702717+peoplhappy@users.noreply.github.com>
2019-07-07 21:17:17 +0800

    init commit
    init commit

---
+++ log-cloud/src/main/java/cn/cloud/log/LogCloudApplication.java
@@ -0,0 +1,13 @@
+package cn.cloud.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogCloudApplication.class, args);
+	}
+
+}
---
+++ log-cloud/src/test/java/cn/cloud/log/LogCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cloud.log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
