79e79f5b4f004568fb5f183f03420baf3a462e3b
peoplhappy <39702717+peoplhappy@users.noreply.github.com>
2019-08-01 09:40:00 +0800

    修改spark配置项

--- log-cloud/src/main/java/cn/cloud/log/test.java
+++ log-cloud/src/main/java/cn/cloud/log/test.java
@@ -3,36 +3,20 @@ package cn.cloud.log;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class test {
-	public static class en1 {
-		private List<String> list = new ArrayList<String>();
-		private String name;
-        public en1(){
-        	list.add("123");
-        }
-		public List<String> getList() {
-			return list;
-		}
-
-		public void setList(List<String> list) {
-			this.list = list;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-	}
-
 	public static void main(String[] args) {
-		en1 d=new en1();
-		List<String> listdist=d.getList();
-		listdist.add("234");
-		for(String l:d.getList()){
-			System.out.println(l);
-		}
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("mytest");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		long starttime=System.currentTimeMillis();
+		JavaRDD<String> lines = sc.textFile("D:/interface-2019.07.31.05-1.log").filter(s->s.contains("ERROR"));
+		long endtime1=System.currentTimeMillis();
+		System.out.println("所耗时间"+(endtime1-starttime));
+		System.out.println(lines.count());
+		long endtime=System.currentTimeMillis();
+		System.out.println("所耗时间"+(endtime-starttime));
 	}
 }
